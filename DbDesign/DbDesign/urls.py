acda800fd2afdeb6e4b24af87a69530248af506c
yokin <yokinzhang@gmail.com>
2017-06-23 11:12:17 +0800

    add demo url

--- DbDesign/DbDesign/urls.py
+++ DbDesign/DbDesign/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^demo/', TemplateView.as_view(template_name="index.html")),
     #url(r'^user/', include('RentMe.urls',namespace='RentMe')),
     #url(r'^index/',)
 ]
