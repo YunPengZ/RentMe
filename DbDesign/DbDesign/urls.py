4736dc59874b44869c813179b8a1eea89cb421f8
yokin <yokinzhang@gmail.com>
2017-06-26 11:08:45 +0800

    重写了.gitignore

--- DbDesign/DbDesign/urls.py
+++ DbDesign/DbDesign/urls.py
@@ -20,9 +20,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^demo/', TemplateView.as_view(template_name="index.html")),
-<<<<<<< HEAD
     #url(r'^user/', include('RentMe.urls',namespace='RentMe')),
     #url(r'^index/',)
-=======
->>>>>>> c152b22036e7f8581ad93268268e1df71f032abe
 ]
