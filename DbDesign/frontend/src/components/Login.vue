0c96a0656023fd8c94d10a763b88dab75d09cfb3
yokin <yokinzhang@gmail.com>
2017-06-26 10:11:29 +0800

    引入 elementUI 修改了Login界面 修改了route

--- DbDesign/frontend/src/components/Login.vue
+++ DbDesign/frontend/src/components/Login.vue
@@ -2,17 +2,19 @@
   <div>
     <el-row>
       <el-col :span="8">
-        <el-form :model="form" label-width="100px">
-          <el-form-item label="手机号">
-            <el-input v-model="form.account" auto-complete="off"></el-input>
-          </el-form-item>
-          <el-form-item label="密码">
-            <el-input type="password" v-model="form.password" auto-complete="off"></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
-          </el-form-item>
-        </el-form>
+        <el-card>
+          <el-form :model="form" label-width="100px">
+            <el-form-item label="手机号">
+              <el-input v-model="form.account" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+              <el-input type="password" v-model="form.password" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+            </el-form-item>
+          </el-form>
+        </el-card>
       </el-col>
     </el-row>
   </div>
