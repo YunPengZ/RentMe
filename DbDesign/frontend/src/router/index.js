a9819d8d273c00bd83f31f47a06c7ac7f43fbdd9
yokin <yokinzhang@gmail.com>
2017-06-26 08:55:20 +0800

    添加.gitignore，取消同步_pycache_ route添加login路由

--- DbDesign/frontend/src/router/index.js
+++ DbDesign/frontend/src/router/index.js
@@ -4,13 +4,39 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 import Hello from '../components/Hello.vue'
+import Login from '../components/Login.vue'
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'Hello',
-      component: Hello
+      component: Hello,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    if ($store.userID.length() == null) {
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  } else {
+    next() // make sure to always call next()!
+  }
+})
+
+export default router
