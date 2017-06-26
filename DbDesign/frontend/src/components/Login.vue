5f04f14251b917abf895329dbfb1f570f2fd0ee1
yokin <yokinzhang@gmail.com>
2017-06-26 10:26:23 +0800

    修改路由权限设置 增加login跳转

--- DbDesign/frontend/src/components/Login.vue
+++ DbDesign/frontend/src/components/Login.vue
@@ -11,7 +11,7 @@
               <el-input type="password" v-model="form.password" auto-complete="off"></el-input>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+              <el-button type="primary" @click="submitForm('form')">提交</el-button>
             </el-form-item>
           </el-form>
         </el-card>
--- DbDesign/frontend/src/router/index.js
+++ DbDesign/frontend/src/router/index.js
@@ -26,7 +26,8 @@ router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
     // this route requires auth, check if logged in
     // if not, redirect to login page.
-    if (router.app.$store.state.user_ID.length() == null) {
+    var auth = true
+    if (auth) {
       next({
         path: '/login',
         query: { redirect: to.fullPath }
