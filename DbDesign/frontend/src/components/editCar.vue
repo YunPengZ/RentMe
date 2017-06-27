fcb283bb2016ab30d8e49cdaa074d9ac4aaeeec4
worthulove <2087447114@qq.com>
2017-06-27 13:24:28 +0800

    添加车辆信息修改页面

---
+++ DbDesign/frontend/src/components/editCar.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+      <el-row type="flex" justify="center">
+          <h1>添加车辆信息</h1>
+          </el-row>
+      <el-row type="flex" justify="center">
+          <el-form :model="ruleForm" label-width="100px">
+            <el-form-item label="车辆号码">
+            <el-input placeholder="请输入内容" v-model="formInline.car_num" >
+            </el-input>
+            </el-form-item>
+            <el-form-item label="车辆型号">
+            <el-input placeholder="请输入内容" v-model="formInline.car_model_id" >
+            </el-input>
+            </el-form-item>
+            <el-form-item label="车辆颜色">
+            <el-input placeholder="请输入内容" v-model="formInline.car_color" >
+
+            </el-input>
+            </el-form-item>
+            <el-form-item label="发动机号">
+            <el-input placeholder="请输入内容" v-model="formInline.car_engine_num" >
+
+            </el-input>
+            </el-form-item>
+            <el-form-item label="车架编号">
+            <el-input placeholder="请输入内容" v-model="formInline.car_frame_num" >
+
+            </el-input>
+            </el-form-item>
+            <el-form-item label="购买日期">
+                    <el-date-picker
+      v-model="formInline.car_buy_date"
+      type="date"
+      :picker-options="pickerOptions0">
+    </el-date-picker>
+
+            </el-input>
+            </el-form-item>
+            <el-form-item label="销售商">
+            <el-input placeholder="请输入内容" v-model="formInline.car_retailer" >
+
+            </el-input>
+            </el-form-item>
+            <el-form-item label="状态">
+            <el-input placeholder="请输入内容" v-model="formInline.car_status" >
+
+            </el-input>
+            </el-form-item>
+            <el-form-item label="保单号">
+            <el-input placeholder="请输入内容" v-model="formInline.car_ins_num" >
+               
+            </el-input>
+            </el-form-item>
+          </el-form>
+       
+      </el-row>
+      <el-row type="flex" justify="center">
+        
+          <el-button type="primary">提交</el-button>
+     
+      </el-row>
+  </div>
+</template>
+<script>
+export default{
+  data () {
+    return {
+      formInline: {
+        car_num: '12987132',
+        car_model_id: '123',
+        car_color: '红色',
+        car_engine_num: '45432',
+        car_frame_num: '47186',
+        car_buy_date: '2016-2-3',
+        car_retailer: '10333',
+        car_status: '已租',
+        car_ins_num: '123456',
+        car_creater: 'kinmin'
+      }
+    }
+  },
+  methods: {
+    handleCheckAllChange (event) {
+    },
+    handleCheckedCitiesChange (value) {
+      let checkedCount = value.length
+      this.checkAll = checkedCount === this.options['car_type'].length
+      this.isIndeterminate = checkedCount > 0 && checkedCount < this.options['car_type'].length
+    }
+  }
+}
+</script>
+<style scoped>
+.text{
+  font-size: 5px;
+  color:grey;
+}
+</style>
