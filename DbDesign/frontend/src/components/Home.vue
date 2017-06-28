9c6b06de0a34136cf209b09625043752818fa641
yokin <yokinzhang@gmail.com>
2017-06-28 23:47:29 +0800

    修改 顶部栏 侧边栏 *完成侧边栏路由 *完成顶部用户显示

--- DbDesign/frontend/src/components/Home.vue
+++ DbDesign/frontend/src/components/Home.vue
@@ -3,10 +3,54 @@
     <el-row class="maxheight">
       <el-col :span="4" class="maxheight">
         <el-menu mode="vertical" default-active="1" class="maxheight">
-          <el-menu-item index="1"><i class="el-icon-message"></i>导航一</el-menu-item>
-          <el-menu-item index="2"><i class="el-icon-message"></i>导航二</el-menu-item>
-          <el-menu-item index="3"><i class="el-icon-message"></i>导航三</el-menu-item>
-          <el-menu-item index="4"><i class="el-icon-message"></i>导航四</el-menu-item>
+          <template>
+            <router-link :to="'/home'" class="text">
+                <el-menu-item index="1" >首页</el-menu-item>
+              </router-link>
+          </template>
+          <template v-if="$store.state.user_Status === '门店管理员'">
+            <el-menu-item-group title="订单信息">
+              <router-link :to="'/home/new_order'" class="text">
+                <el-menu-item index="2" >添加订单</el-menu-item>
+              </router-link>
+              <router-link :to="'/home/order'" class="text">
+                <el-menu-item index="3" >订单管理</el-menu-item>
+              </router-link>
+            </el-menu-item-group>
+            <el-menu-item-group title="车辆信息">
+              <router-link :to="'/home/addCar'" class="text">
+                <el-menu-item index="4" >添加车辆</el-menu-item>
+              </router-link>
+              <router-link :to="'/home/carManage'" class="text">
+                <el-menu-item index="5" >车辆管理</el-menu-item>
+              </router-link>
+            </el-menu-item-group>
+          </template>
+          <template v-if="$store.state.user_Status === '违章信息管理员'">
+            <el-menu-item-group title="违章信息">
+              <router-link :to="'/home/violation_info'" class="text">
+                <el-menu-item index="2" >违章录入</el-menu-item>
+              </router-link>
+            </el-menu-item-group>
+          </template>
+          <template v-if="$store.state.user_Status === '大中华区管理员'">
+            <el-menu-item-group title="总览">
+              <router-link :to="'/home/analysis'" class="text">
+                <el-menu-item index="2" >统计信息</el-menu-item>
+              </router-link>
+            </el-menu-item-group>
+            <el-menu-item-group title="管理">
+              <router-link :to="'/home/storeManage'" class="text">
+                <el-menu-item index="3" >门店及管理员</el-menu-item>
+              </router-link>
+              <router-link :to="'/home/addManager'" class="text">
+                <el-menu-item index="4" >添加管理员</el-menu-item>
+              </router-link>
+              <router-link :to="'/home/addStore'" class="text">
+                <el-menu-item index="5" >添加门店</el-menu-item>
+              </router-link>
+            </el-menu-item-group>
+          </template>
         </el-menu>
       </el-col>
       <el-col :span="20" class="maxheight background">
@@ -28,6 +72,9 @@
   }
 </script>
 <style scoped>
+  .text{
+    text-decoration: none;
+  }
   .background{
     background-color: #d1dbe5;
   }
