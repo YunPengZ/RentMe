4626f748729a12f04db331c8f6556c0e93876c25
SpeaklessAmande <11414045@ahu.edu.cn>
2017-06-28 16:59:33 +0800

    添加回来了

---
+++ DbDesign/RentMe/serializers.py
@@ -0,0 +1,63 @@
+# coding:utf-8
+from rest_framework import serializers
+from rest_framework.serializers import *
+from RentMe.models import *
+
+#关联Model_info
+class ModelSerializer(serializers.ModelSerializer):
+    car_model = serializers.PrimaryKeyRelatedField(many=True, queryset=car_info.objects.all())
+    class Meta:
+        model = model_info
+        fields = ('model_id','car_model_id','car_model','car_type','car_brand','car_series','car_issue_date','car_config_model','car_seats_num','car_doors','car_fuel_type','car_gearbox_type','car_displacement','car_fuel_num','car_drive_way','car_engine_intake','car_skylight','car_tank_capa','car_voicebox','car_seats_type','car_reverse_radar','car_airbag','car_dvd','car_gps','car_deposit','car_day_price','car_time_out_price','car_over_kilo_price','record_delete_status')
+#关联user_info
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = user_info
+        fields = ('user_id','user_name','user_sex','user_age','user_ident','user_tel','user_office','user_addr','user_post','user_email','user_record_create_time','record_delete_status')
+
+#关联driving_license
+class DrivingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = driving_license
+        fields = ('drive_id','user_drive','drive_type','drive_age','drive_start_date','drive_end_date')
+#关联admin_info
+class AdminSerializer(serializers.HyperlinkedModelSerializer):
+    #store_manage = serializers.HyperlinkedRelatedField(many=True,view_name='adminn-detail',read_only=True)
+    class Meta:
+        model = admin_info
+        fields = ('admin_id','admin_tel','admin_pas','admin_name','store_manage','admin_sex','admin_age','admin_record_create_time','admin_type','admin_email','admin_ident',)
+#关联store_info
+class StoreSerializer(serializers.HyperlinkedModelSerializer):
+    #store_admin = serializers.ReadOnlyField(source='store_admin.admin_name')
+    class Meta:
+        model = store_info
+        fields = ('store_id','store_addr','store_tel','store_start_time','store_admin','store_record_create_time','record_delete_status')
+#关联car_info
+class CarSerializer(serializers.ModelSerializer):
+   # car_model_id = serializers.ReadOnlyField(source='car_model_id.model_id')
+    class Meta:
+        model =car_info
+        fields = ('car_id','car_num','car_model_id','car_color','car_engine_num','car_engine_num','car_frame_num','car_buy_date','car_retailer','car_status','car_ins_num','record_create_admin','car_record_create_time','record_delete_status')
+
+
+
+#class DrivingSerializer(serializers.Serializer):
+    #pk = serializers.IntegerField(read_only=True)
+   # user_drive = serializers.CharField(required=False,allow_blank=True,max_length=12)
+    #drive_type = serializers.CharField(required=False,default='C级驾照',max_length=12)
+    #drive_age = serializers.IntegerField()
+    #drive_start_date = serializers.DateField()
+    #drive_end_date = serializers.DateField()
+
+    #def create(self, validated_data):
+        #数据合法，返回并创建driving_license实例
+       # return driving_license.objects.create(**validated_data)
+
+    #def update(self, instance, validated_data):
+        #instance.user_drive = validated_data.get('user_drive',instance.user_drive)
+        #instance.drive_type = validated_data.get('user_drive',instance.user_drive)
+        #instance.drive_age = validated_data.get('user_drive',instance.user_drive)
+        #instance.drive_start_date = validated_data.get('drive_start_date',instance.drive_start_date)
+        #instance.drive_end_date = validated_data.get('drive_end_date',instance.drive_end_date)
+        #instance.save()
+       # return instance
---
+++ DbDesign/RentMe/views.py
@@ -0,0 +1,181 @@
+# coding:utf-8
+from rest_framework import status,generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication
+from RentMe import models
+from RentMe.serializers import *
+
+#license_info查询
+@api_view(['GET','POST'])
+def license_list(request,format=None):
+    #展示或者创建driving_license
+    if request.method == 'GET':
+        licenses = driving_license.objects.all()
+        serializer = DrivingSerializer(licenses,many=True)
+        #print('get')
+        #print(serializer.data)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = DrivingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            #print('post')
+            #print(Response(serializer.data))
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def license_detail(request,pk,format=None):
+    try:
+        license = driving_license.objects.get(pk=pk)
+    except driving_license.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = DrivingSerializer(license)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = DrivingSerializer(license,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        license.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+#model_info查询
+@api_view(['GET','POST'])
+def model_info_list(request,format=None):
+    #展示或者创建driving_license
+    if request.method == 'GET':
+        models_ = model_info.objects.all()
+        serializer = ModelSerializer(models_,many=True)
+        #print('get')
+        #print(serializer.data)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            #print('post')
+            #print(Response(serializer.data))
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def model_info_detail(request,pk,format=None):
+    try:
+        models_ = model_info.objects.get(pk=pk)
+    except model_info.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = ModelSerializer(models_)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ModelSerializer(models_,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        models_.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+#user_info查询
+@api_view(['GET','POST'])
+def user_info_list(request,format=None):
+    #展示或者创建driving_license
+    if request.method == 'GET':
+        users = user_info.objects.all()
+        serializer = UserSerializer(users,many=True)
+        #print('get')
+        #print(serializer.data)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            #print('post')
+            #print(Response(serializer.data))
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def user_info_detail(request,pk,format=None):
+    try:
+        users = user_info.objects.get(pk=pk)
+    except user_info.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = UserSerializer(users)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = UserSerializer(users,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        users.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET','POST'])
+def login(request):
+    if request.method == 'POST':
+        tel = request.POST.get('admin_tel','18855121479')
+        pas = request.POST.get('admin_pas','wozhua')
+        admin = admin_info.objects.filter(admin_tel__exact=tel,admin_pas__exact=pas)
+        #filter返回QuerySet集合对象，遍历获得唯一元素
+        if admin:
+            for admin_item in admin:
+                #print(admin_item)
+                admin_serializer = AdminSerializer(admin_item)
+            return Response(admin_serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'GET':
+        admins = admin_info.objects.all()
+        serializer = AdminSerializer(admins,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    return  Response(status=status.HTTP_404_NOT_FOUND)
+
+class ModelList(generics.ListCreateAPIView):
+    queryset = model_info.objects.all()
+    serializer_class = ModelSerializer
+
+#管理员列表
+class AdminList(generics.ListCreateAPIView):
+    queryset = admin_info.objects.all()
+    serializer_class = AdminSerializer
+
+#管理详情
+class AdminDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = admin_info.objects.all()
+    serializer_class = AdminSerializer
+
+#门店列表
+class StoreList(generics.ListCreateAPIView):
+    queryset = store_info.objects.all()
+    serializer_class = StoreSerializer
+
+    #def perform_create(self, serializer):
+       # serializer.save(store_admin=self.request.)
+
+#门店详情
+class StoreDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = store_info.objects.all()
+    serializer_class = StoreSerializer
+
+#车辆列表
+class CarList(generics.ListCreateAPIView):
+    queryset = car_info.objects.all()
+    serializer_class=CarSerializer
+   # def perform_create(self, serializer):
+       # print(self.request.POST)
+       ## model_at = model_info.objects.get(pk=self.request.POST.car_model_id)
+       # serializer.save(model_at)
+
+#车辆详情
+class CarDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = car_info.objects.all()
+    serializer_class=CarSerializer
