3986f44348c753f7c7d5bbc84a1cc76d1517cfb9
Kin-min <2087447114@qq.com>
2017-06-28 09:15:11 +0800

    登录界面修改

--- DbDesign/frontend/src/components/Login.vue
+++ DbDesign/frontend/src/components/Login.vue
@@ -1,8 +1,10 @@
-<template>
-  <div>
-    <el-row>
-      <el-col :span="6">
-        <el-form :model="form" label-width="100px">
+<template  >
+  <div style="background-color:#050304;height:100%;">
+    <el-row  >
+      <el-col :offset="9" :span="6">
+        <!--<img src="../assets/car.jpg"></img>-->
+        <el-card class="position">
+        <el-form :model="form" >
           <el-form-item label="手机号">
             <el-input v-model="form.account" auto-complete="off"></el-input>
           </el-form-item>
@@ -13,6 +15,7 @@
             <el-button type="primary" @click="login">提交</el-button>
           </el-form-item>
         </el-form>
+        </el-card>
       </el-col>
     </el-row>
   </div>
@@ -53,7 +56,10 @@
 
     }
 </script>
-
-<style scoped>
-
+<style>
+.position{
+  margin-top:100px;
+}
 </style>
+
+   
