ccc21c72a65d5d0b4a5c85db5bd01ed4e174ad2c
SniperChen <pengshichenno.1@live.com>
2017-06-29 13:10:47 +0800

    整理后台接口

--- DbDesign/RentMe/urls.py
+++ DbDesign/RentMe/urls.py
@@ -4,35 +4,43 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from RentMe import views
 
 
+#关于URL使用说明
+#如果添加了正在使用的url请放在正在使用区
+
+
 urlpatterns = {
+    #>>>>>>>>>>>>>>>>正在使用的url
     url(r'^licenses/$',views.license_list),#驾驶证
-    url(r'licenses/(?P<pk>[0-9]+)$',views.license_detail),
-    #url(r'^model/$',views.ModelList.as_view(),name='model-list'),
-    url(r'^model/$',views.model_info_list),#车型
-    url(r'^model/(?P<pk>[0-9]+)$',views.model_info_detail),
-    url(r'^car/$',views.CarList.as_view()),#车型
-    url(r'^car/(?P<pk>[0-9]+)$',views.CarDetail.as_view()),
-    url(r'^user/$',views.user_info_list),#用户
-    url(r'^user/(?P<pk>[0-9]+)$',views.user_info_detail),
+    url(r'^car/$',views.car_list),#车型
+    url(r'^user/$',views.user_list),#用户
     url(r'^admins/$',views.AdminList.as_view()),#管理员
-    url(r'^admins/(?P<pk>[0-9]+)$',views.AdminDetail.as_view(),name='admin-detail'),
-    url(r'^stores/$',views.StoreList.as_view()),#门店
-    url(r'^stores/(?P<pk>[0-9]+$)',views.StoreDetail.as_view()),
-    url(r'^order/$',views.OrderList.as_view()),#租车订单
-    url(r'^order/(?P<pk>[0-9]+$)',views.OrderDetail.as_view()),
-    url(r'^order/$',views.OrderList.as_view()),		
-    url(r'^order/type$',views.get_car_info_by_type),		
-    url(r'^order/date$',views.get_car_info_by_date),		
-    url(r'^order/pay$',views.order_pay),		
-    url(r'^order/date_type$',views.get_car_info_by_dateAndStore),
-    url(r'^relet/$',views.ReletList.as_view()),#续租记录
-    url(r'^relet/(?P<pk>[0-9]+$)',views.ReletDetail.as_view()),
-    url(r'^illegal/$',views.IllegalList.as_view()),
-    url(r'^illegal/(?P<pk>[0-9]+$)',views.IllegalDetail.as_view()),
-    #url(r'^cars/$',views.CarList.as_view()),#车辆
-    #url(r'^cars/(?P<pk>[0-9]+)$',views.CarDetail.as_view()),
+    url(r'^stores/$',views.store_list),#门店
+    url(r'^relet/$',views.relet_list),#续租记录
+    url(r'^illegal/$',views.illegal_list),
     url(r'^login/$',views.login),#登录
 
+    url(r'^order/$',views.OrderList.as_view()),#租车订单
+    #url(r'^order/$',views.OrderList.as_view()),
+    url(r'^order/type$',views.get_car_info_by_type),
+    url(r'^order/date$',views.get_car_info_by_date),
+    url(r'^order/pay$',views.order_pay),
+    url(r'^order/date_type$',views.get_car_info_by_dateAndStore),
+
+
+    #>>>>>>>>>>>>>>>未使用的url
+    #url(r'licenses/(?P<pk>[0-9]+)$',views.license_detail),
+    #url(r'^model/$',views.ModelList.as_view(),name='model-list'),
+    #url(r'^model/$',views.model_list),#车型
+    #url(r'^model/(?P<pk>[0-9]+)$',views.model_info_detail),
+    #url(r'^cars/$',views.CarList.as_view()),#车辆
+    #url(r'^cars/(?P<pk>[0-9]+)$',views.CarDetail.as_view()),
+    #url(r'^car/(?P<pk>[0-9]+)$',views.CarDetail.as_view()),
+    #url(r'^user/(?P<pk>[0-9]+)$',views.user_info_detail),
+    #url(r'^admins/(?P<pk>[0-9]+)$',views.AdminDetail.as_view(),name='admin-detail'),
+    #url(r'^order/(?P<pk>[0-9]+$)',views.OrderDetail.as_view()),
+    #url(r'^stores/(?P<pk>[0-9]+$)',views.StoreDetail.as_view()),
+    #url(r'^relet/(?P<pk>[0-9]+$)',views.ReletDetail.as_view()),
+    #url(r'^illegal/(?P<pk>[0-9]+$)',views.IllegalDetail.as_view()),
 }
 
 
