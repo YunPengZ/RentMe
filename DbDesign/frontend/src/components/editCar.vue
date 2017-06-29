80410449f8ebc8781957fba4bf6e28de3a6ebbbb
Kin-min <2087447114@qq.com>
2017-06-29 08:39:55 +0800

    登录界面全局数组赋值

--- DbDesign/frontend/src/components/editCar.vue
+++ DbDesign/frontend/src/components/editCar.vue
@@ -4,7 +4,7 @@
           <h1>修改车辆信息</h1>
           </el-row>
       <el-row type="flex" justify="center">
-          <el-form :model="ruleForm" label-width="100px">
+          <el-form :model="formInline" label-width="100px">
             <el-form-item label="车辆号码">
             <el-input placeholder="请输入内容" v-model="formInline.car_num" >
             </el-input>
@@ -57,37 +57,66 @@
       </el-row>
       <el-row type="flex" justify="center">
         
-          <el-button type="primary">提交</el-button>
+          <el-button type="primary" @click="updateCar(this.$route.params.id)">提交</el-button>
      
       </el-row>
   </div>
 </template>
 <script>
+import axios from 'axios'
 export default{
   data () {
     return {
       formInline: {
-        car_num: '12987132',
-        car_model_id: '123',
-        car_color: '红色',
-        car_engine_num: '45432',
-        car_frame_num: '47186',
-        car_buy_date: '2016-2-3',
-        car_retailer: '10333',
-        car_status: '已租',
-        car_ins_num: '123456',
-        car_creater: 'kinmin'
+        // car_num: '12987132',
+        // car_model_id: '123',
+        // car_color: '红色',
+        // car_engine_num: '45432',
+        // car_frame_num: '47186',
+        // car_buy_date: '2016-2-3',
+        // car_retailer: '10333',
+        // car_status: '已租',
+        // car_ins_num: '123456',
+        // car_creater: 'kinmin'
       }
     }
   },
+  created () {
+    var self = this
+    var id = self.$route.params.id
+    axios.get('/test/car/?format=json', {
+      params: {
+        car_num: id
+      }
+    })
+         .then(function (response) {
+           self.formInline = response.data[0]
+         })
+         .catch(e => {
+           this.errors.push(e)
+         })
+  },
   methods: {
-    handleCheckAllChange (event) {
-    },
-    handleCheckedCitiesChange (value) {
-      let checkedCount = value.length
-      this.checkAll = checkedCount === this.options['car_type'].length
-      this.isIndeterminate = checkedCount > 0 && checkedCount < this.options['car_type'].length
-    }
+        updateCar (car_num) {
+          var self = this
+          console.log(self.form.account)
+          axios.post('/test/login/?format=json', {
+            admin_tel: self.form.account,
+            admin_pas: self.form.password
+          })
+            .then(function (response) {
+              self.$message('登录成功')
+              self.$store.state.user_Name = response.data['admin_name']
+              self.$store.state.user_ID = response.data['user_ID']
+              self.$store.state.user_Status = response.data['user_Status']
+              self.$router.push('/home')
+              self.$router.go(1)
+            })
+            .catch(e => {
+              self.$message('账号或者密码错误，请重新输入')
+              this.errors.push(e)
+            })
+        }
   }
 }
 </script>
